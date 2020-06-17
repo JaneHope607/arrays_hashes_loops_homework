79a5d9e4ed0a58094954adb783968f8617a1db51
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-17 20:20:00 +0100

    #7 excercise_b

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -66,13 +66,13 @@ users["Erik"][:lottery_numbers]
 # 4. Get the species of Avril's pet Monty
 
 # 5. Get the smallest of Erik's lottery numbers
-p users["Erik"][:lottery_numbers].min
+users["Erik"][:lottery_numbers].min
 
 # 6. Return an array of Avril's lottery numbers that are even
 
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
-
+users["Erik"][:lottery_numbers].append(7)
 
 # 8. Change Erik's hometown to Edinburgh
 users ["Erik"][:home_town] = "Edinburgh"
