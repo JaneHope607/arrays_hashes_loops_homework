e3b51cfa0d21d4983ebc3335cd68c5d83bbc52ed
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-17 18:51:24 +0100

    #3 excercise_b

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -61,7 +61,7 @@ p users["Jonathan"][:twitter]
 p users["Erik"][:home_town]
 
 # 3. Get the array of Erik's lottery numbers
-
+p users["Erik"][:lottery_numbers]
 
 # 4. Get the species of Avril's pet Monty
 
