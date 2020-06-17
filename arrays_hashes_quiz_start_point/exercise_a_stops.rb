eee2ce1c6a08f6f7470900c64c712adc1987573e
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-17 18:37:36 +0100

    #10_excercise_a_stops

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -22,11 +22,14 @@ stops.delete_at(2)
 p stops.length()
 
 #8. Show as many ways as you can to return "Falkirk High" from the array?
-p stops
+
 
 #9. Reverse the positions of the stops in the array
 
 
 #10 Print out all the stops using a for loop
+for stop in stops
+    p stop
+end
 
 
